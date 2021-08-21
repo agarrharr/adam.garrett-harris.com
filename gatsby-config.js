0801d43205eb35457ae5a35905178fc745ba8804
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-08-21 14:17:58 -0600

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-31980509-3`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
